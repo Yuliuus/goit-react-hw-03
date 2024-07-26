9022b482b849508c388f81f634f1b2ffa39d2562
Yuliuus <yuliiamarkovska@gmail.com>
2024-07-26 12:12:22 +0300

    update save to local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,17 @@ import { useEffect, useState } from "react";
 import ContactForm from "../ContactForm/ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContactsList = localStorage.getItem("savedContacts");
+    if (savedContactsList !== null) {
+      return JSON.parse(savedContactsList);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("savedContacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const [filter, setFilter] = useState("");
 
@@ -18,6 +23,7 @@ function App() {
     setContacts((prevContList) => {
       return [...prevContList, newContact];
     });
+    console.log(newContact);
   };
 
   const handleDelete = (contactId) => {
@@ -29,10 +35,6 @@ function App() {
   const filterContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
 
   return (
     <div>
